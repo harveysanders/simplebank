4a437330bf355b15b5f0658f5e25027525f5a905
Harvey Sanders <sanders.harvey@gmail.com>
2022-11-21 12:58:47 -0500

    update db connections

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -13,7 +13,7 @@ var testQueries *Queries
 
 const (
 	dbDriver = "postgres"
-	dbSourse = "postgresql://root:sY54tji6XqFl@localhost:5433/simple_bank?sslmode=disable"
+	dbSourse = "postgresql://root:devsecret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 func TestMain(m *testing.M) {
